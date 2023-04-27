9f575a824d9962df491f4bdb17be4b5eb562d820
Alex Seifert <alex@alexseifert.com>
2023-04-27 15:34:01 +0200

    Fix the project root for dev

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 
-const projectRoot = path.resolve(__dirname, '..', '..');
+const projectRoot = process.env.NODE_ENV === 'development' ? path.resolve(__dirname, '..') : path.resolve(__dirname, '..', '..');
 
 export const FEED_FILE = path.resolve(projectRoot, 'feeds.json');
 export const FEED_CACHE_FILE = path.resolve(projectRoot, 'feedCache.json');
