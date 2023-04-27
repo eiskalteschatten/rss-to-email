13cc84c87f22f7a8132c1598681f2278090213db
Alex Seifert <alex@alexseifert.com>
2023-04-27 11:09:28 +0200

    Cleanup

--- src/errors.ts
+++ src/errors.ts
@@ -4,7 +4,6 @@ export async function handleError(error: Error | string): Promise<void> {
   console.error(error);
 
   const mailer = new Mailer();
-
   const emailBody = typeof error === 'string' ? error : error.message;
   await mailer.sendErrorMail(emailBody);
 }
