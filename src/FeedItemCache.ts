1ba29500042cfa31a3254ebf6af704e075dffa7a
Alex Seifert <alex@alexseifert.com>
2023-04-27 11:48:42 +0200

    Finish the cacheFeedItems function

--- src/FeedItemCache.ts
+++ src/FeedItemCache.ts
@@ -1,12 +1,32 @@
 import cron from 'node-cron';
 import { Item } from 'rss-parser';
+import fs from 'node:fs';
 
 import config from '../config';
 import { handleError } from './errors';
+import { FEED_CACHE_FILE } from './constants';
+import { FeedCacheItem } from './interfaces';
 
 class FeedItemCache {
-  async cacheFeedItem(item: Item): Promise<void> {
+  async cacheFeedItems(items: Item[]): Promise<void> {
+    try {
+      const feedCacheItems = await this.readCacheFile();
 
+      for (const item of items) {
+        const feedCacheItem: FeedCacheItem = {
+          guid: item.guid,
+          pubDate: item.pubDate,
+          link: item.link,
+        };
+
+        feedCacheItems.push(feedCacheItem);
+      }
+
+      await fs.promises.writeFile(FEED_CACHE_FILE, JSON.stringify(feedCacheItems));
+    }
+    catch (error) {
+      handleError(error);
+    }
   }
 
   setupCleanupCronjob(): void {
@@ -19,11 +39,37 @@ class FeedItemCache {
   }
 
   private async cleanupCache(): Promise<void> {
-    console.log('Cleaning up the feed item cache...');
+    try {
+      console.log('Cleaning up the feed item cache...');
 
-    // TODO
+      if (!fs.existsSync(FEED_CACHE_FILE)) {
+        // Do nothing because there is no cache to clean up.
+        return;
+      }
 
-    console.log('The feed item cache has been cleaned up.');
+      // TODO
+
+      console.log('The feed item cache has been cleaned up.');
+    }
+    catch (error) {
+      handleError(error);
+    }
+  }
+
+  private async readCacheFile(): Promise<FeedCacheItem[]> {
+    try {
+      let feedCacheItems: FeedCacheItem[] = [];
+
+      if (fs.existsSync(FEED_CACHE_FILE)) {
+        const feedCacheString = await fs.promises.readFile(FEED_CACHE_FILE, 'utf8');
+        feedCacheItems = JSON.parse(feedCacheString);
+      }
+
+      return feedCacheItems;
+    }
+    catch (error) {
+      handleError(error);
+    }
   }
 }
 
