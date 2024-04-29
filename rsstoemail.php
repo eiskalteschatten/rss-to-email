53846cecb32cc7a6d937289df28b6dd65fe9d7fb
Alex Seifert <alex@alexseifert.com>
2024-04-29 09:27:21 +0200

    Start parsing the feeds file

--- rsstoemail.php
+++ rsstoemail.php
@@ -1,3 +1,17 @@
 <?php
 
-require_once 'config.php';
+require_once "config.php";
+
+$feeds = simplexml_load_file("feeds.opml") or die("Unable to find the \"feeds.opml\" file!");
+
+foreach ($feeds->body->outline as $folder) {
+    echo "Folder: {$folder['title']}\n";
+
+    foreach ($folder->outline as $feed) {
+        echo "Feed: {$feed['title']}\n";
+        echo "XML URL: {$feed['xmlUrl']}\n";
+        echo "HTML URL: {$feed['htmlUrl']}\n";
+    }
+
+    echo "\n\n";
+}
