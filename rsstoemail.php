f162b280d4f5bbc6d4b1dd1bec7e43e8e20008ac
Alex Seifert <alex@alexseifert.com>
2024-04-29 09:37:34 +0200

    Start fetching the RSS feeds

--- rsstoemail.php
+++ rsstoemail.php
@@ -1,16 +1,29 @@
 <?php
-
 require_once "config.php";
 
 $feeds = simplexml_load_file("feeds.opml") or die("Unable to find the \"feeds.opml\" file!");
 
 foreach ($feeds->body->outline as $folder) {
-    echo "Folder: {$folder['title']}\n";
+    $folderTitle = (string) $folder['title'];
+    echo "Folder: {$folderTitle}\n";
 
     foreach ($folder->outline as $feed) {
-        echo "Feed: {$feed['title']}\n";
-        echo "XML URL: {$feed['xmlUrl']}\n";
-        echo "HTML URL: {$feed['htmlUrl']}\n";
+        $feedTitle = (string) $feed['title'];
+        echo "Feed: {$feedTitle}\n";
+        $xmlUrl = (string) $feed['xmlUrl'];
+        echo "XML URL: {$xmlUrl}\n";
+        $htmlUrl = (string) $feed['htmlUrl'];
+        echo "HTML URL: {$htmlUrl}\n";
+
+        $rss = simplexml_load_file($xmlUrl);
+
+        if ($rss === false) {
+            // TODO: email the error
+            echo "Feed \"${xmlUrl}\" could not be loaded!\n";
+            continue;
+        }
+
+        var_dump($rss);
     }
 
     echo "\n\n";
