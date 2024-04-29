4258e21f6822a34e069d4781fcea7ed7ed288076
Alex Seifert <alex@alexseifert.com>
2024-04-29 11:04:19 +0200

    Sending feed items via email now works

--- rsstoemail.php
+++ rsstoemail.php
@@ -1,5 +1,12 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 require_once "config.php";
+
+require "PHPMailer/Exception.php";
+require "PHPMailer/PHPMailer.php";
+require "PHPMailer/SMTP.php";
 
 $twoWeeksAgo = new DateTime();
 $twoWeeksAgo->modify('-2 weeks');
@@ -28,6 +35,7 @@ foreach ($feeds->body->outline as $folder) {
 
         foreach ($rss->channel->item as $item) {
             $itemTitle = (string) $item->title;
+            $itemTitle = html_entity_decode($itemTitle, ENT_QUOTES, 'UTF-8');
             $itemLink = (string) $item->link;
             $itemDescription = (string) $item->description;
             $itemPubDateStr = (string) $item->pubDate;
@@ -43,15 +51,42 @@ foreach ($feeds->body->outline as $folder) {
                 $subject = "{$itemTitle} :: Folder: {$folderTitle} :: Feed: {$feedTitle}";
 
                 $body = "<html><body>";
-                $body .= "<h1>{$itemTitle}</h1>";
-                $body .= "<p><a href=\"{$itemLink}\">{$itemTitle}</a></p>";
-                $body .= "<p>{$itemPubDate->format('Y-m-d H:i:s')}</p>";
+                $body .= "<h1><a href=\"{$itemLink}\">{$itemTitle}</a></h1>";
+                $body .= "<p>";
+                $body .= "<a href=\"{$htmlUrl}\">{$feedTitle}</a>";
+                $body .= "&nbsp;&nbsp;∙&nbsp;&nbsp;";
+                $body .= "<span style=\"text-transform: uppercase; opacity: .6;\">{$itemPubDate->format('F j, Y H:i:s')}</span>";
+                $body .= "</p>";
                 $body .= $itemDescription;
                 $body .= "<p><a href=\"{$itemLink}\">Read more...</a></p>";
                 $body .= "</body></html>";
 
                 echo "Subject: {$subject}\n";
                 echo "Body: {$body}\n";
+
+                $mail = new PHPMailer;
+                $mail->isSMTP();
+                $mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+                $mail->Host = $EMAIL_SMTP_HOST;
+                $mail->Port = $EMAIL_SMTP_PORT;
+                $mail->SMTPSecure = 'tls';
+                $mail->SMTPAuth = true;
+                $mail->Username = $EMAIL_SMTP_USER;
+                $mail->Password = $EMAIL_SMTP_PASSWORD;
+                $mail->setFrom($EMAIL_SMTP_FROM_EMAIL, $feedTitle);
+                $mail->addAddress($EMAIL_TO, $EMAIL_TO_NAME);
+                $mail->Subject = $subject;
+                $mail->msgHTML($body);
+                $mail->AltBody = 'HTML messaging not supported';
+
+                if (!$mail->send()) {
+                    echo "Mailer Error: " . $mail->ErrorInfo;
+                }
+                else {
+                    echo "Message sent!";
+                    // Sleep for a second so as not to overwhelm the receiving server and (hopefully) avoid being marked as spam
+                    sleep(1);
+                }
             }
         }
     }
